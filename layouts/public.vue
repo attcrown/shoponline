033df6018681743dacbe0f26f4dedc6cb4caa9c2
Mr.Vongvaris Chawawiwat <62502710+attcrown@users.noreply.github.com>
2024-01-26 22:45:08 +0700

    set store public

--- layouts/public.vue
+++ layouts/public.vue
@@ -7,6 +7,24 @@
 </template>
 <script>
 export default {
-    name: 'publicLayout'
+    name: 'publicLayout',
+    mounted() {
+        this.beforeCreate();
+    },
+    methods: {
+        beforeCreate() {
+            const auth = this.$fireModule.auth();
+            auth.onAuthStateChanged((user) => {
+                if (user) {
+                    if (user.emailVerified) {
+                        this.handleUserData(user);
+                    }
+                }
+            });
+        },
+        handleUserData(user) {
+            this.$store.commit('SET_USER', user);
+        }
+    },
 }
 </script>
