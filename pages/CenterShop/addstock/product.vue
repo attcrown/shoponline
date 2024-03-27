f8b6967d6eb361ea3bad423c7fda9937eac3343f
Mr.Vongvaris Chawawiwat <62502710+attcrown@users.noreply.github.com>
2024-03-27 18:52:06 +0700

    update Review

--- pages/CenterShop/addstock/product.vue
+++ pages/CenterShop/addstock/product.vue
@@ -1,35 +1,41 @@
 <template>
-    <div class="text-center" :class="!$store.state.deviceMode ? 'd-flex' : ''">
-        <div class="mt-10" :style="!$store.state.deviceMode ? 'width: 50%' : 'width: 100%'">
-            <div>
-                Add item
-            </div>
-            <v-form ref="form" v-model="valid" lazy-validation class="mx-5">
-                <div>
-                    <v-file-input v-model="jpgUpload" counter multiple small-chips label="เพิ่มรูปภาพ"
-                        accept="image/png, image/jpeg, image/bmp"
-                        :rules="[v => v.length > 0 || 'This field is required']" required>
-                    </v-file-input>
-                </div>
-                <div class="d-flex">
-                    <v-text-field v-model="items.name" :counter="50" :rules="nameRules" label="Name" class="me-3"
-                        required>
-                    </v-text-field>
+    <div>
+        <div :class="!$store.state.deviceMode ? 'd-flex' : ''">
+            <div class="mt-10 mx-5" :style="!$store.state.deviceMode ? 'width: 30%' : ''">
+                <v-banner class="text-center" color="white" elevation="10">
+                    Add item <v-icon>mdi-plus-circle</v-icon>
+                </v-banner>            
+                <v-form ref="form" class="mt-5" v-model="valid" lazy-validation>
+                    <div>
+                        <v-file-input v-model="jpgUpload" 
+                            :counter="6" multiple small-chips label="เพิ่มรูปภาพ"
+                            accept="image/png, image/jpeg, image/bmp"
+                            :rules="[v => v.length > 0 || 'This field is required']" required>
+                        </v-file-input>
+                    </div>
+                    <div class="d-flex">
+                        <v-text-field v-model="items.name" :counter="50" :rules="nameRules" label="Name"
+                            required>
+                        </v-text-field>
 
-                </div>
-            </v-form>
-            <v-btn class="mx-2" color="success" @click="validate">
+                    </div>
+                </v-form>
+            </div>
+
+            <v-divider :vertical="!$store.state.deviceMode" color="black" class="mt-3"></v-divider>
+            
+            <div class="mt-10" :style="!$store.state.deviceMode ? 'width: 70%' : ''">
+                <review ref="review"></review>
+            </div>        
+            <LoadingItem v-if="loading"></LoadingItem>
+        </div>
+        <div class="mx-5">
+            <v-btn color="success" @click="validate">
                 submit
             </v-btn>
         </div>
-
-        <v-divider :vertical="!$store.state.deviceMode" color="black" class="mt-3"></v-divider>
-        
-        <div class="mt-10" :style="!$store.state.deviceMode ? 'width: 50%' : 'width: 100%'">
-            <review ref="review"></review>
-        </div>
-        <LoadingItem v-if="loading"></LoadingItem>
     </div>
+    
 </template>
 <script>
 import { processImg } from '~/services/img-sizing';
@@ -52,21 +58,31 @@ export default {
         LoadingItem,
         review
     },
+
+    watch: {
+        'items.name': async function () {
+            if (this.$refs.review) {
+                this.$refs.review.name = this.items.name;                
+            }
+        },
+        'jpgUpload': async function () {
+            if (this.$refs.review) {
+                let imgSizing = []
+                for (const img in this.jpgUpload) {
+                    let result = await processImg(this.jpgUpload[img])
+                    imgSizing.push({ src: URL.createObjectURL(result) })
+                }
+                this.$refs.review.itemsImg = imgSizing
+            }
+        }
+    },
+
     mounted() {
         this.loading = false
     },
     methods: {
         async validate() {
             this.$refs.form.validate()
-            let imgSizing = []
-
-            for (const img in this.jpgUpload) {
-                let result = await processImg(this.jpgUpload[img])
-                imgSizing.push({ src: URL.createObjectURL(result) })
-            }
-
-            console.log(imgSizing)
-            this.$refs.review.itemsImg = imgSizing
         },
     }
 }
--- pages/CenterShop/addstock/review.vue
+++ pages/CenterShop/addstock/review.vue
@@ -1,25 +1,61 @@
 <template>
-    <div class="text-center">
-        Review
-        <div v-if="itemsImg.length > 0" style="width: 100%; height: 100%;">
-            <v-carousel show-arrows>
-                <v-carousel-item
-                    v-for="(item,i) in itemsImg"
-                    :key="i"
-                    height="100%"
-                    :src="item.src">
-                </v-carousel-item>
-            </v-carousel>
+    <div class="mx-5">
+        <v-banner class="text-center" color="white" elevation="10">
+            Review <v-icon>mdi-eye</v-icon>
+        </v-banner>        
+        <div class="mt-5" :class="!$store.state.deviceMode ? 'd-flex' : '' ">
+            <div :style="$store.state.deviceMode ? 
+                'width: 100%; height: 380px' : 'width: 350px'">
+                <v-carousel 
+                    v-if="itemsImg.length > 0"
+                    class="carousel-item"
+                    cycle
+                    height="350"
+                    hide-delimiter-background
+                    show-arrows-on-hover>
+                    <v-carousel-item
+                        v-for="(item,i) in itemsImg"
+                        :key="i">
+                        <img :src="item.src" 
+                            style="width: 350px; height: 350px;"
+                            class="carousel-img"/>
+                    </v-carousel-item>
+                </v-carousel>
+            </div>
+            <div :class="!$store.state.deviceMode ? 'ms-5' : ''">
+                <h4 class="max-length">{{ name }}</h4>
+                <p>{{ title }}</p>
+            </div>
         </div>
-        {{ itemsImg }}
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-            itemsImg:[]
+            itemsImg:[],
+            name: '',
+            title: '',
         }
     }
 }
 </script>
+
+<style>
+.carousel-item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.carousel-img {
+  object-fit: contain; /* หรือ object-fit: cover; ตามความเหมาะสม */
+}
+
+.max-length {
+    max-width: 28ch; /* กำหนดความยาวของข้อความให้เป็น 20 ตัวอักษร */
+    word-wrap: break-word;
+    white-space: pre-wrap;
+}
+
+</style>
