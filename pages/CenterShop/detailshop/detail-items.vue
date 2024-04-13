2f70bf064e64f870d6f4931a31ba5572877dba2e
Mr.Vongvaris Chawawiwat <62502710+attcrown@users.noreply.github.com>
2024-04-13 18:47:19 +0700

    update detail-items

--- pages/CenterShop/detailshop/detail-items.vue
+++ pages/CenterShop/detailshop/detail-items.vue
@@ -1,39 +1,70 @@
 <template>
     <div>
-        details items
-        {{ items.seller }}
-        {{ items.stockItems }}
+        <div :class="!$store.state.deviceMode ? 'd-flex justify-center mt-5' : ''">
+            <div :style="!$store.state.deviceMode ? 'width: 1200px; display: flex' : ''">            
+                <div :style="!$store.state.deviceMode ? 'width: 400px;' : 'width: 100%;'">
+                    <controlReviewImg ref="controlReview"></controlReviewImg>
+                </div>
+                <div style="width: 100%;">
+                    <controlDetail ref="controlDetail"></controlDetail>
+                </div>
+            </div>
+        </div>
+
+        <AlertButtom ref="AlertButtom"></AlertButtom>
     </div>
 </template>
 <script>
+import AlertButtom from '~/components/AlertButtom.vue';
+import controlDetail from '~/modules/controlDetail.vue';
+import controlReviewImg from '~/modules/controlReviewImg.vue';
 export default {
     data() {
         return {
-            items : []
+            items: []
         };
     },
+    components: {
+        AlertButtom, controlDetail, controlReviewImg
+    },
     async mounted() {
-        await this.$store.state.detail_item;
         this.searchDetailShop();
     },
     methods: {
-        searchDetailShop() {
+        async searchDetailShop() {
             // read path
-            this.path = this.$router.currentRoute.hash
-            this.pathSubId = this.path.split('#')
-            const pathId = this.pathSubId[1]
-            const pathDocs = this.pathSubId[2]
+            let path = this.$router.currentRoute.hash
+            let pathSubId = path.split('#')
+            const pathId = pathSubId[1]
+            const pathDocs = pathSubId[2]
 
-            this.items = this.$store.state.detail_item
             const db = this.$fire.database;
-            // reatlTime search id
-            db.ref(`items/${pathId}`).on('value', (snapshot) => {
-                if (!snapshot.exists()) return
-                
-                const data = snapshot.val();
-                this.items = {...this.items , ...data}
+            const dbDocs = this.$fire.firestore;
+
+            try {
+                db.ref(`items/${pathId}`).on('value', (snapshot) => {
+                    const data = snapshot.val();
+                    
+                    this.items.seller = data.seller
+                    this.items.stockItems = data.stockItems
+                    this.items.view = data.view
+                })
+
+                const dataDocs = await dbDocs.collection(`items/`).doc(pathDocs).get()
+                const itemsDocs = dataDocs.data()
+                this.items = { ...this.items, ...itemsDocs}
+
+               
                 console.log(this.items)
-            })
+                this.$refs.controlDetail.items = this.items
+                this.$refs.controlReview.itemsImg = this.items.imgs
+            } catch (error) {
+                this.items = []
+                this.$refs.AlertButtom.snackbar = true
+                this.$refs.AlertButtom.colorAlart = 'red'
+                this.$refs.AlertButtom.text = 'ไม่พบข้อมูลสินค้าในระบบ'
+                this.$refs.AlertButtom.icon = 'mdi mdi-alert-circle'
+            }
         }
     },
 }
