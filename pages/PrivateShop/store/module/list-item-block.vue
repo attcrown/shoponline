1de1da66610ab5c76b4d5696fb263a61c77baa68
Mr.Vongvaris Chawawiwat <62502710+attcrown@users.noreply.github.com>
2024-04-27 14:48:09 +0700

    change

--- pages/PrivateShop/store/module/list-item-block.vue
+++ pages/PrivateShop/store/module/list-item-block.vue
@@ -1,15 +1,13 @@
 <template>
-    <div>
-        <div class="text-center d-flex pb-4">
-            <v-btn @click="all">
-                all
-            </v-btn>
-        </div>
-
-        <v-expansion-panels v-model="panel" multiple>
-            <v-expansion-panel v-for="(item, i) in items" :key="i">
-                <v-expansion-panel-header>Header {{ item }}</v-expansion-panel-header>
-                <v-expansion-panel-content>
+    <div class="m-3">
+        
+        <v-expansion-panels inset dark>
+            <v-expansion-panel v-for="(item, i) in 5" :key="i">
+                <v-expansion-panel-header 
+                    color="#0240aa">
+                    Item
+                </v-expansion-panel-header>
+                <v-expansion-panel-content style="background-color: white; color: #0240aa" >
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
                     et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
                     aliquip ex ea commodo consequat.
@@ -18,21 +16,3 @@
         </v-expansion-panels>
     </div>
 </template>
-<script>
-export default {
-    data() {
-        return {
-            panel: [],
-            items: 5,
-        }
-    },
-    methods: {
-        all() {
-            this.panel = [...Array(this.items).keys()].map((k, i) => i)
-        },
-        none() {
-            this.panel = []
-        },
-    },
-}
-</script>
