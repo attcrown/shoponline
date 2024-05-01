d125ad96ecad00f2a2221dd90e772013f4960b3a
Mr.Vongvaris Chawawiwat <62502710+attcrown@users.noreply.github.com>
2024-05-01 17:08:19 +0700

    add test data store

---
+++ services/testData/constStore.js
@@ -0,0 +1,99 @@
+import { formatDatetime } from '../formatDatetime.js'
+export const TESTDATASTORE = [
+    {
+        id : "EDti-1231sda-213",
+        name : 'Cymera-Photo Editor Camera Cymera-Photo Editor Camera',
+        block : 10,
+        createdAt : formatDatetime('2022-10-10', '12:00'),                    
+        cost : 0.55,
+        contract : 1095
+    },
+    {
+        id : "EDti-1231sda-21323asdawd",
+        name : 'Cymera-Photo Editor Camera Cymera-Photo Editor Camera',
+        block : 25,
+        createdAt : formatDatetime('2023-11-10', '15:00'),
+        cost : 0.55,
+        contract : 1095
+    },
+    {
+        id : "EDti-1231sda-21311-sdaa-aasdsd",
+        name : 'Cymera-Photo Editor Camera Cymera-Photo Editor Camera',
+        block : 15,
+        createdAt : formatDatetime('2023-12-10', '16:00'),
+        cost : 0.55,
+        contract : 1095
+    },
+    {
+        id : "EDti-1231da-1311-sdaa-aasdsd",
+        name : 'Cymera-Photo Editor Camera Cymera-Photo Editor Camera',
+        block : 15,
+        createdAt : formatDatetime('2024-01-10', '07:00'),
+        cost : 0.55,
+        contract : 30
+    },
+    {
+        id : "EDti-1231sda-21311-sdasd",
+        name : 'Cymera-Photo Editor Camera Cymera-Photo Editor Camera',
+        block : 15,
+        createdAt : formatDatetime('2024-01-10', '12:00'),
+        cost : 0.55,
+        contract : 60
+    },
+    {
+        id : "EDti-1231sda-311-sdasd",
+        name : 'Cymera-Photo Editor Camera',
+        block : 50,
+        createdAt : formatDatetime('2024-05-10', '12:00'),
+        cost : 0.55,
+        contract : 30
+    },
+    {
+        id : "EDti-1231sda-213233",
+        name : 'Cymera-Photo Editor Camera Cymera-Photo Editor Camera',
+        block : 10,
+        createdAt : formatDatetime('2022-10-10', '12:00'),                    
+        cost : 0.55,
+        contract : 1095
+    },
+    {
+        id : "Dti-1231sda-21323asdawd",
+        name : 'Cymera-Photo Editor Camera Cymera-Photo Editor Camera',
+        block : 25,
+        createdAt : formatDatetime('2023-11-10', '15:00'),
+        cost : 0.55,
+        contract : 1095
+    },
+    {
+        id : "Ei-1231sda-21311-sdaa-aasdsd",
+        name : 'Cymera-Photo Editor Camera Cymera-Photo Editor Camera',
+        block : 15,
+        createdAt : formatDatetime('2023-12-10', '16:00'),
+        cost : 0.55,
+        contract : 1095
+    },
+    {
+        id : "EDti-1231da-11-sdaa-aasdsd",
+        name : 'Cymera-Photo Editor Camera Cymera-Photo Editor Camera',
+        block : 15,
+        createdAt : formatDatetime('2024-01-10', '07:00'),
+        cost : 0.55,
+        contract : 30
+    },
+    {
+        id : "EDti-31sda-21311-sdasd",
+        name : 'Cymera-Photo Editor Camera Cymera-Photo Editor Camera',
+        block : 15,
+        createdAt : formatDatetime('2024-01-10', '12:00'),
+        cost : 0.55,
+        contract : 60
+    },
+    {
+        id : "EDti-1231sda-311-sdd",
+        name : 'Cymera-Photo Editor Camera',
+        block : 50,
+        createdAt : formatDatetime('2024-05-10', '12:00'),
+        cost : 0.55,
+        contract : 30
+    }
+]
