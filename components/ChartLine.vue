e42426a727c086ba9d562cb6451dcf834e16bf81
Acrownrity <ittatting@gmail.com>
2024-08-19 21:48:37 +0700

    fix bug chart success

--- components/ChartLine.vue
+++ components/ChartLine.vue
@@ -1,62 +1,19 @@
-<template>
-    <div>
-        <client-only>
-            <BarChart :data="chartData" :options="options" />
-        </client-only>
-    </div>
-</template>
-
 <script>
+import { Line } from 'vue-chartjs';
 export default {
-    data() {
-        return {
-            chartData: null,
-            options: null,
-        }
-    },
-    created() {
-        this.setChartData()
-        this.setOptions()
-    },
-    methods: {
-        setChartData() {
-            this.chartData = {
-                labels: [1, 2, 3, 4, 5 ,6 ,7 ,8],
-                datasets: [
-                    {
-                        label: 'My First Dataset',
-                        data: [1, 4, 6, 3, 0.5 ,10 ,5 ,3],
-                        backgroundColor: [
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(75, 192, 192, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgb(75, 192, 192)',
-                            'rgb(75, 192, 192)',
-                            'rgb(75, 192, 192)',
-                            'rgb(75, 192, 192)',
-                            'rgb(75, 192, 192)',
-                            'rgb(75, 192, 192)',
-                            'rgb(75, 192, 192)',
-                            'rgb(75, 192, 192)'
-                        ],
-                        borderWidth: 1,
-                    },
-                ],
-            }
+    extends: Line,
+    props: {
+        chartData: {
+            type: Object,
+            default: null
         },
-        setOptions() {
-            this.options = {
-                responsive: true,
-                maintainAspectRatio: false
-            }
+        chartOptions: {
+            type: Object,
+            default: null
         }
+    },
+    mounted() {
+        this.renderChart(this.chartData, this.chartOptions)
     }
 }
 </script>
--- pages/PrivateShop/store/module/price-items-all.vue
+++ pages/PrivateShop/store/module/price-items-all.vue
@@ -4,23 +4,33 @@
             <v-card elevation="4"
                 :class="$store.state.deviceMode ? '' : 'p-5 rounded-xxl'"
                 :style="$store.state.deviceMode ? 'width: 100%;' : 'width: 60%;'">
-                <ChartLine></ChartLine>
+                <ChartLine :chartData="chartData" />
             </v-card>
         </div>
     </div>
 </template>
 <script>
 import ChartLine from '~/components/ChartLine.vue';
+
 export default {
     data() {
-        return { }
+        return { 
+            chartData: {
+                labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+                datasets: [
+                    {
+                        label: 'ราคาสินค้าทั้งหมด',
+                        data: [0, 2, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 37, 40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                        borderColor: 'rgba(255, 99, 132, 1)',
+                        borderWidth: 1
+                    }
+                ]
+            }
+        }
     },
     components: {
-        ChartLine
-    },
-    mounted() {
-
+        
     }
-
 }
 </script>
